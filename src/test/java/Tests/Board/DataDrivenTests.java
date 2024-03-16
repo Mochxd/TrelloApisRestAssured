cd893e8a22c52082670c6573eb476526715a7140
Mochxd <mm2900@fayoum.edu.eg>
2024-03-16 21:39:38 +0200

    Update Board Tests

--- src/test/java/Tests/Board/DataDrivenTests.java
+++ src/test/java/Tests/Board/DataDrivenTests.java
@@ -2,9 +2,12 @@ package Tests.Board;
 
 import Tests.Base.BaseTests;
 import Utilities.DataProviders;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.http.ContentType;
 
-import jdk.jfr.Description;
 import org.testng.annotations.Test;
 
 import static EndPoints.Urls.createBoardUrl;
@@ -13,6 +16,8 @@ import static io.restassured.RestAssured.given;
 public class DataDrivenTests extends BaseTests {
     @Test(priority = 1, dataProvider = "allData", dataProviderClass = DataProviders.class)
     @Description("creating new Board with External Data in the Excel Sheet")
+    @Story("Board")
+    @Severity(SeverityLevel.CRITICAL)
     public void testCreateBoardWithExternalData(String name, String desc, String defaultLists){
         boolean list = Boolean.parseBoolean(defaultLists);
         given()
